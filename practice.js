561900629c5c4c73789196f503d51047c0a626b7
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-06-26 17:26:51 -0600

    Afternoon Practice: Through Extra Practice # 8.5

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  return arr[0];
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[arr.length-1];
+}
 
 
 
@@ -47,7 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+  for(i=0; i < family.length; i++){
+    alert(family[i]);
+  }
+}
 
 
 
@@ -62,7 +70,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
+function reversedLooper(letters){
+  for(i=letters.length-1; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
 
 
 
@@ -77,7 +89,13 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  let newArr = [];
+  for(i=0; i < nums.length; i++){
+   if (nums[i]%2===0){newArr.push(nums[i])}
+  }
+  return newArr;
+}
 
 
 
@@ -105,7 +123,25 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
   Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+function divider(numbersArray){
+  let evenArr = [];
+  let oddArr = [];
+  let finalArray = [];
+ 
+  for(i=0; i < numbersArray.length; i++){
+      
+      if (numbersArray[i] % 2 === 0){
+          evenArr.push(numbersArray[i])
+      }
+
+      else if (numbersArray[i] % 2 !== 0){
+          oddArr.push(numbersArray[i])
+      }
+      
+  }
+  return finalArray = [evenArr, oddArr];
+}
+ 
 
 
 
@@ -126,8 +162,11 @@ var getRandomArbitrary = function() {
   If it is, return true, if it's not, return false
 */
 
-//Code Here
 
+
+  function finder(arr) {
+    return arr.indexOf(getRandomArbitrary()) >= 0;
+  }
 
 
 ////////// PROBLEM 8 //////////
@@ -155,7 +194,26 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   addItem() --> [];
 */
 
-//Code Here
+
+function removeItem(myGroceryList, item){
+  let emptyArr = [];
+
+  if (myGroceryList && item){
+
+  if (myGroceryList.indexOf(item) >= 0){
+    myGroceryList.splice(item, 1);
+  }
+  
+  return myGroceryList;
+
+}
+else {return emptyArr}
+
+}
+
+
+
+
 
 
 
